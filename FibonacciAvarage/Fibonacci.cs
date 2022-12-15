cc0b77184bddba762bedd5a8fd79b1763d5f7381
Candar TOZAN <candar.tozan@outlook.com>
2022-12-15 23:00:22 +0300

    Add project files.

---
+++ FibonacciAvarage/Fibonacci.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FibonacciAvarage
+{
+    public class Fibonacci
+    {
+        public Fibonacci()
+        {
+        }
+
+        int Fib(int level)
+        {
+            if (level == 1 || level == 0)
+                return 0;
+            if (level == 2)
+                return 1;
+            return Fib(level - 1) + Fib(level - 2);
+        }
+
+        public double Avarage(double level)
+        {
+            double sum = 0;
+
+            for (int i = 0; i <= level; i++)
+            {
+                sum = sum + Fib(i);
+            }
+
+            return sum / level;
+        }
+    }
+}
---
+++ FibonacciAvarage/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FibonacciAvarage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fib = new Fibonacci();
+
+            Console.WriteLine(fib.Avarage(5));
+            Console.WriteLine(4.35);
+
+        }
+    }
+}
